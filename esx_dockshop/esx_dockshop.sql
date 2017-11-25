10ad2dc8d340ecf86efb8a0c10c11f718027d688
RPF Studio <groupe-esp-fr@outlook.fr>
2017-11-25 16:37:24 +0100

    Update vehicle

--- esx_dockshop/esx_dockshop.sql
+++ esx_dockshop/esx_dockshop.sql
@@ -77,6 +77,19 @@ CREATE TABLE `dock` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `dock` (name, model, price, category) VALUES
-  ('Toro','toro',15000,'dock')
+INSERT INTO dock (name, model, price, category) VALUES
+  ('Seashark','seashark',1500,'dock'),
+  ('Seashark2','seashark2',1500,'dock'),
+  ('Yacht Seashark','seashark3',1500,'dock'),
+  ('Suntrap','suntrap',1500,'dock')
+  ('Dinghy','dinghy',2500,'dock'),
+  ('Dinghy2 ','dinghy2',2500,'dock'),
+  ('Yacht Dinghy','dinghy4',1500,'dock'),
+  ('Tropic','tropic',10000,'dock'),
+  ('Yacht Tropic','tropic2',10000,'dock'),
+  ('Squalo','squalo',12000,'dock'),
+  ('Yacht Toro','toro2',15000,'dock'),
+  ('Toro','toro',15000,'dock'),
+  ('Jetmax','jetmax',17500,'dock'),
+  ('Volier Marquis','marquis',45500,'dock')
 ;
